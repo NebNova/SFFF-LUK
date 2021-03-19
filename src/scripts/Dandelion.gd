ab91f21e70866c2ba3897ec811b4119c86102200
Neb <merlinx50@gmail.com>
2021-03-19 18:51:57 -0400

    V0.1.1

---
+++ src/scripts/Dandelion.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export (int) var speed = 500 # Sets generic speed
+
+var velocity = Vector2() # Sets genertic movement speed to zero
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
