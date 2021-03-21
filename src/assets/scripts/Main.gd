0a4a2483a3637c47654709aec3affa90ebd8c540
Neb <merlinx50@gmail.com>
2021-03-21 17:49:11 -0400

    v0.18.0

--- src/assets/scripts/Main.gd
+++ src/assets/scripts/Main.gd
@@ -2,10 +2,26 @@ extends Node2D
 
 var dandelion = preload("res://src/characters/Unicorn.tscn") # Preloads Dandy
 var dandyPOS = Vector2(200,320) # Sets Dandys spawn vector
-onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+onready var musicTimer: Timer = get_node("musicTimer")
+onready var anim_player: AnimationPlayer = get_node("MusicPlayer")
+var musicTrack = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	anim_player.play("startMusic")
 	var dandy = dandelion.instance() # Set Dandy scene instance
 	dandy.position = dandyPOS # Set Dandy spawn position
 	add_child(dandy) # Adds Dandy as child node
+
+
+func _on_musicTimer_timeout() -> void:
+	if musicTrack == 0:
+		anim_player.play("midMusic")
+		musicTrack += 1
+	elif musicTrack == 1:
+		anim_player.play("endMusic")
+		musicTrack += 1
+		musicTimer.stop()
+	else:
+		pass
