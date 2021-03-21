bd086c91f37b8805bb91e7ce0f7a5796aadf7e28
Neb <merlinx50@gmail.com>
2021-03-21 18:30:48 -0400

    v1.0.1 - Minor UI fixes

--- src/UI/MenuButton.gd
+++ src/UI/MenuButton.gd
@@ -1,5 +1,6 @@
 extends Button
 
 func _on_Play_button_up() -> void:
+	PlayerData.score = 0
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://src/Main Scenes/MainScreen.tscn")
--- src/UI/PlayFromPause.gd
+++ src/UI/PlayFromPause.gd
@@ -1,5 +1,6 @@
 extends Button
 
 func _on_Play_button_up() -> void:
+	PlayerData.score = 0
 	get_tree().paused = false
 	get_tree().change_scene("res://src/Main Scenes/MainScreen.tscn")
